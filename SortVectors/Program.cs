803cc35ece5a14cbc097377bc91ab5fdce90f702
tommaso.zammarchi <tommaso.zammarchi@PC7324>
2020-10-28 11:05:41 +0100

    Primo passo: aggiunta metodo ordinamento e individuazione del minimo

---
+++ SortVectors/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SortVectors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] vett = new int [10] { 8, 4, -9, 5, 2 , 1, 7, 45, 76, 89};
+
+            Ordina(vett);
+            int min = Minimo(vett);
+
+            Console.WriteLine("Il valore minimo è: " + min);
+
+        }
+
+        static void Ordina(int [] v)
+        {
+            Array.Sort(v);
+        }
+
+        static int Minimo(int [] v)
+        {
+            return v[0];
+        }
+            
+    }
+}
